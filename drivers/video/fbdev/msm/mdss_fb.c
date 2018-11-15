24e34ebd86fdac5b497f451797cf97ac860f0f8a
AyushR1 <ayushrathore12501@gmail.com>
2018-11-15 21:13:55 +0530

    Revert "REmove boost"
    This reverts commit 606d99b9c0dd903a3a8fcb360a0be0c940fa7cdc.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -46,6 +46,8 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <sync.h>
 #include <sw_sync.h>
 
@@ -5061,6 +5063,8 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		cpu_general_boost_kick(64);
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
