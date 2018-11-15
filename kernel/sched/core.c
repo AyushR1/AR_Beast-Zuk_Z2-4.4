78098ef372e58c76eb131e6e4fe497e1bcb28b70
AyushR1 <ayushrathore12501@gmail.com>
2018-11-15 12:01:46 +0530

    Revert "sched/core: Fix remote wakeups"
    This reverts commit 0f34d21d2581c10e00e9ca44c858de0187e5fdb3.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1944,15 +1944,9 @@ void sched_ttwu_pending(void)
 	lockdep_pin_lock(&rq->lock);
 
 	while (llist) {
-		int wake_flags = 0;
-
 		p = llist_entry(llist, struct task_struct, wake_entry);
 		llist = llist_next(llist);
-
-		if (p->sched_remote_wakeup)
-			wake_flags = WF_MIGRATED;
-
-		ttwu_do_activate(rq, p, wake_flags);
+		ttwu_do_activate(rq, p, 0);
 	}
 
 	lockdep_unpin_lock(&rq->lock);
@@ -2008,11 +2002,9 @@ void scheduler_ipi(void)
 	irq_exit();
 }
 
-static void ttwu_queue_remote(struct task_struct *p, int cpu, int wake_flags)
+static void ttwu_queue_remote(struct task_struct *p, int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
-
-	p->sched_remote_wakeup = !!(wake_flags & WF_MIGRATED);
 
 	if (llist_add(&p->wake_entry, &cpu_rq(cpu)->wake_list)) {
 		if (!set_nr_if_polling(rq->idle))
@@ -2052,14 +2044,14 @@ bool cpus_share_cache(int this_cpu, int that_cpu)
 }
 #endif /* CONFIG_SMP */
 
-static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
+static void ttwu_queue(struct task_struct *p, int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 
 #if defined(CONFIG_SMP)
 	if (sched_feat(TTWU_QUEUE) && !cpus_share_cache(smp_processor_id(), cpu)) {
 		sched_clock_cpu(cpu); /* sync clocks x-cpu */
-		ttwu_queue_remote(p, cpu, wake_flags);
+		ttwu_queue_remote(p, cpu);
 		return;
 	}
 #endif
@@ -2218,8 +2210,7 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags,
 
 	note_task_waking(p, wallclock);
 #endif /* CONFIG_SMP */
-
-	ttwu_queue(p, cpu, wake_flags);
+	ttwu_queue(p, cpu);
 stat:
 	ttwu_stat(p, cpu, wake_flags);
 out:
