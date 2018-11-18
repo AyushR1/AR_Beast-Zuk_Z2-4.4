a8feb3ec0b613b1017e2d0b13ff24fd39f1fb8c2
AyushR1 <ayushrathore12501@gmail.com>
2018-11-18 09:40:18 +0530

    block more wakelocks
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;hal_bluetooth_lock;epoll_InputReader_file;event4;epoll_InputReader_file:event4"
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;hal_bluetooth_lock;epoll_InputReader_file;event4;epoll_InputReader_file:event4;IPCRTR_dsps_rx;epoll_healthd_file:NETLINK;epoll_healthd_epollfd;epoll_InputReader_file:event1;epoll_InputReader_file:event2;epoll_InputReader_epollfd;epoll_system_server_file:[timerfd8_system_server];epoll_system_server_file:[timerfd5_system_server];epoll_system_server_file:[timerfd6_system_server]"
 
-#define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		142
+#define LENGTH_LIST_WL				1000
+#define LENGTH_LIST_WL_DEFAULT		502
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
